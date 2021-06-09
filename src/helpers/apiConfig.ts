d1b8896675661566d9a497c41cfd6d96d0f0b16d
Fadia Tarek Ahmed <fadia.tarek@flairstech.com>
2021-06-09 17:45:49 +0200

    code clean up

--- src/helpers/apiConfig.ts
+++ src/helpers/apiConfig.ts
@@ -1,11 +1,11 @@
 export const createApiconfig = (method?: string, body?: any): any => {
 
-  const token = 'fadia.tarek19@gmail.com'
+  const token = 'fadia.tarek19@gmail.com';
     const config = {
       headers: new Headers({
         'Authorization': `Basic ${token}`,
-        // Accept: 'application/json',
-        // 'content-type': 'application/json',
+        Accept: 'application/json',
+        'content-type': 'application/json',
       }),
       mode: 'no-cors',
       method: method || 'GET',
